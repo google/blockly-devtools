a2e8365585b7c133e75b95bef599d7fbc169407b
Celine Choo <celinechoo@celinechoo.mtv.corp.google.com>
2017-05-31 16:58:29 -0700

    New Block Popup Feature
    Can now run devtools on NW.js! (Added simple instructions to readme.)
    Added a function to app_controller.js to create a popup whenever a new
    block is created.
    Imported jQuery.
    Modified BlockFactory.showStarterBlock by adding parameters.
    
    Not yet functional features
    
    Disallowing user to create a new block under a preexisting block type
    name.
    Refining popup form to be more useful and relevant (i.e. user may not
    necessarily need starter text; maybe add other starter items to choose
    from, perhaps a text box.) (This is much less of a priority, however.)
    
    Summary
    
    Getting used to code base.
    Created a semi/mostly-functional popup.
    Basics of NW.js working.

---
+++ src/new_block_dialog_controller.js
@@ -0,0 +1,63 @@
+/**
+ * @license
+ * Blockly Demos: Block Factory
+ *
+ * Copyright 2017 Google Inc.
+ * https://developers.google.com/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+/**
+ * @fileoverview NewBlockDialogController manages the UI for creating new blocks and
+ * projects.
+ *
+ * @author celinechoo (Celine Choo)
+ */
+class NewBlockDialogController {
+  constructor(dialogName, blockLibraryController) {
+    this.name = dialogName;
+    this.view = new NewBlockDialogView();
+    this.blockLibraryController = blockLibraryController;
+
+    $('#block_name').change(() => {
+      this.checkDuplicate();
+    });
+
+    this.view.on('exit', () => {
+      BlockFactory.showStarterBlock(this.view.inputType, this.view.blockText,
+          this.view.blockName);
+    });
+  }
+
+  /**
+   * This shows a dialog to create a new block.
+   *
+   * @param {string} firstLoad Whether new block is the first block upon creating
+   * a new project, or not.
+   */
+  showNewBlockDialog(firstLoad) {
+    this.view.openDialog(firstLoad);
+  }
+
+  /**
+   * Checks for duplicate block type. If user is trying to create a block under a type name
+   * that already exists in the library, warn user.
+   */
+   checkDuplicate() {
+    let hasDuplicate = this.blockLibraryController.has($("#block_name").val());
+    this.view.showWarning(hasDuplicate);
+   }
+}
---
+++ src/new_block_dialog_view.js
@@ -0,0 +1,108 @@
+/**
+ * @license
+ * Blockly Demos: Block Factory
+ *
+ * Copyright 2017 Google Inc.
+ * https://developers.google.com/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var Emitter = require('component-emitter');
+
+/**
+ * @fileoverview NewBlockDialogView deals with the UI for creating new blocks and
+ * projects.
+ *
+ * @author celinechoo (Celine Choo)
+ */
+class NewBlockDialogView {
+  constructor() {
+    this.firstLoad = true;
+    this.blockName = 'block_type';
+    this.inputType = 'input_statement';
+    this.blockText = 'MY_BLOCK';
+    Emitter(NewBlockDialogView.prototype);
+
+    $('#exit').click(() => {
+      if (this.firstLoad) {
+        BlockFactory.showStarterBlock(this.inputType, this.blockText, this.blockName);
+      }
+      this.closeDialog();
+    });
+
+    $('#submit_block').click((event) => {
+      // Gathers and renders blocks properly in devtools editor.
+      event.preventDefault();
+
+      this.blockName = $('#block_name').val();
+      this.inputType = $('input[name="input_type"]:checked').val();
+      this.blockText = $('#block_text').val();
+
+      this.emit('exit', this);
+
+      this.closeDialog();
+    });
+  }
+
+  /**
+   * Opens new block dialog popup.
+   * @param {boolean} firstLoad Whether dialog is opening upon the first launch
+   * of the application.
+   */
+  openDialog(firstLoad) {
+    this.firstLoad = firstLoad;
+    $('#popup').css('display', 'inline');
+  }
+
+  /**
+   * Closes new block popup, resets fields in form.
+   */
+  closeDialog() {
+    $('#popup').css('display', 'none');
+    this.resetPopup();
+  }
+
+  /**
+   * Displays warning message for duplicate block type, only if there is a
+   * duplicate.
+   */
+  showWarning(hasDuplicate) {
+    if (hasDuplicate) {
+      $('#block_name').css('border', '1px solid red');
+      $('#warning_text').css('display', 'inline');
+      $('#submit_block').attr('disabled','disabled');
+    } else {
+      $('#block_name').css('border', '1px solid gray');
+      $('#warning_text').css('display', 'none');
+      $('#submit_block').removeAttr('disabled');
+    }
+  }
+
+  /**
+   * Resets popup form fields to be empty upon creating another block.
+   */
+  resetPopup() {
+    $('#block_name').val('');
+    $('#block_text').val('');
+    $('input[name="input_type"]').attr('checked','checked');
+
+    this.blockName = 'block_type';
+    this.inputType = 'input_statement';
+    this.blockText = 'MY_BLOCK';
+
+    this.showWarning(false);
+  };
+}
