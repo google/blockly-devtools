e86363f603300ba6a98a846f24bce19b0a779f94
Celine Choo <celinechoo@celinechoo.mtv.corp.google.com>
2017-05-31 16:58:29 -0700

    Adding some dysfunctional files for devtools v2

---
+++ src/devtoolsv2.js
@@ -0,0 +1,17 @@
+/**
+ *  Devtools v2.
+ *  
+ *  celinechoo (Celine Choo)
+ *  
+ *  Currently as a prototype.
+ *  TODO: Make popup, file tree view, and import/export functions.
+ */
+
+// Initial testing
+console.log("wao");
+
+$(document).ready(function(){
+  var x = prompt("ready:","bob");
+  console.log('new name: ' + x);
+});
+
