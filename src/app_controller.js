a28ae3b75c33f07e283ea26c68c947b32b498136
Celine Choo <celinechoo@gogle.com>
2017-06-08 16:31:49 -0700

    Stylistic edits, minor functional changes.

--- src/app_controller.js
+++ src/app_controller.js
@@ -2,7 +2,7 @@
  * @license
  * Blockly Demos: Block Factory
  *
- * Copyright 2017 Google Inc.
+ * Copyright 2016 Google Inc.
  * https://developers.google.com/blockly/
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -54,9 +54,9 @@ AppController = function() {
   this.exporter =
       new BlockExporterController(this.blockLibraryController.storage);
 
-  // Initialize Window Manager
-  this.fileManagerController =
-      new FileManagerController('fileManager', this.blockLibraryController);
+  // Initialize New Block Dialog Controller
+  this.newBlockDialogController =
+      new NewBlockDialogController('blockDialog', this.blockLibraryController);
 
   // Map of tab type to the div element for the tab.
   this.tabMap = Object.create(null);
@@ -527,8 +527,7 @@ AppController.prototype.assignBlockFactoryClickHandlers = function() {
         }
       });
 
-  document.getElementById('createNewBlockButton')
-    .addEventListener('click', function() {
+  $('#createNewBlockButton').click(() => {
       // If there are unsaved changes warn user, check if they'd like to
       // proceed with unsaved changes, and act accordingly.
       var proceedWithUnsavedChanges =
@@ -537,7 +536,7 @@ AppController.prototype.assignBlockFactoryClickHandlers = function() {
         return;
       }
 
-      AppController.prototype.createBlocklyInitPopup(false);
+      this.createBlocklyInitPopup(false);
       self.blockLibraryController.setNoneSelected();
 
       // Close the Block Library Dropdown.
@@ -747,5 +746,5 @@ AppController.prototype.init = function() {
  * @param {boolean} firstLoad Whether function is being called on page load.
  */
 AppController.prototype.createBlocklyInitPopup = function(firstLoad) {
-  this.fileManagerController.newBlock(firstLoad);
+  this.newBlockDialogController.showNewBlockDialog(firstLoad);
 };
