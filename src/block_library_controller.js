a7889fc24214cd0afa27a73de6dbe77aeb2f8e2b
Sage Vousé <sagev@google.com>
2017-06-16 09:30:53 -0700

    fixed missing param, changed commit email

--- src/block_library_controller.js
+++ src/block_library_controller.js
@@ -61,6 +61,7 @@ BlockLibraryController = function(blockLibraryName, opt_blockLibraryStorage) {
 /**
 * Assigns a tree instance such that the navigation tree will be edited
 * with changes to block storage
+* @param {!JSTree} tree the main navigation tree
 */
 BlockLibraryController.prototype.setTree = function(tree){
   this.tree = tree;
