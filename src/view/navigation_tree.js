77a8c939754453a1bea906d3a63217b40f4bc57b
Sage Vousé <svouse@gmail.com>
2017-07-05 18:31:57 -0700

    fixed an extra t in a comment

--- src/view/navigation_tree.js
+++ src/view/navigation_tree.js
@@ -166,7 +166,7 @@ class NavigationTree {
   /**
    * Creates menu for right click functionality.
    * @return {!Object} The right click menu for the nodes in the tree.
-   * //TODO: add right click functtionality to tree
+   * //TODO: add right click functionality to tree
    */
    createMenu() {
     const items = {
