455e77c4d31bb62abcaf75ffb285655a61673423
Sage Vousé <svouse@gmail.com>
2017-07-24 10:49:54 -0700

    updated json getter

--- src/model/workspace_configuration.js
+++ src/model/workspace_configuration.js
@@ -77,11 +77,12 @@ class WorkspaceConfiguration extends Resource {
   /**
    * Gets the JSON object necessary to represent the workspace configuration in
    *     the navigation tree.
-   * @return {!Object} The tree-specific JSON representation of the workspace
-   *     configuration.
+   * @return {!Object} The JSON representation of the workspace configuration.
    */
-  getTreeJson() {
-    throw "unimplemented: getTreeJson";
+  getJson() {
+    const workspaceConfigJson = $.extend(true, super.getJson(),
+      {'id': PREFIXES.WORKSPACE_CONFIG});
+    return workspaceConfigJson;
   }
 
   /**
