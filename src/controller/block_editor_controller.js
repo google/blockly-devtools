bfba142538ed9b2c932da4a13f2eda44e7251c22
Sage Vousé <svouse@gmail.com>
2017-08-04 14:08:44 -0700

    small debug change

--- src/controller/block_editor_controller.js
+++ src/controller/block_editor_controller.js
@@ -176,8 +176,8 @@ class BlockEditorController {
     const rootBlock = FactoryUtils.getRootBlock(this.view.editorWorkspace);
     const blockXml = '<xml>' + Blockly.Xml.domToText(Blockly.Xml.blockToDom(rootBlock)) + '</xml>';
     currentBlock.setXml(Blockly.Xml.textToDom(blockXml));
-    currentBlock.json = FactoryUtils.getBlockDefinition(
-          'JSON', this.view.editorWorkspace);
+    currentBlock.json = '[' + FactoryUtils.getBlockDefinition(
+          'JSON', this.view.editorWorkspace).join(',\n') + ']';
   }
 
   /**
